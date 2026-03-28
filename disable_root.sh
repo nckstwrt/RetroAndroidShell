59cc6d7d22dd41ca0dedc4f5c1a348250dc9a0ff
nckstwrt <nick.g.stewart@gmail.com>
2026-03-28 19:57:14 +0000

    Fake being charged whilst in root

--- disable_root.sh
+++ disable_root.sh
@@ -1,5 +1,6 @@
 svc power stayon false
 dumpsys deviceidle enable
+dumpsys battery reset
 killall -9 bftpd
 killall -9 socat
 (sleep 1 && /data/local/tmp/reset_prop ro.secure 1 && start adbd) & stop adbd
--- enable_root.sh
+++ enable_root.sh
@@ -4,6 +4,7 @@ chmod +x /data/local/tmp/bftpd
 chmod +x /data/local/tmp/reset_prop
 svc power stayon true
 dumpsys deviceidle disable
+dumpsys battery set usb 1
 PATH=$PATH:/data/local/tmp nohup /data/local/tmp/socat -T 86400 TCP-LISTEN:4444,reuseaddr,fork,keepalive EXEC:'/system/bin/sh -i',pty,stderr,setsid,ctty,echo=0,sigint,sane </dev/null >/dev/null 2>&1 &
 nohup /data/local/tmp/bftpd -D -c /data/local/tmp/bftpd.conf &
 (sleep 1 && /data/local/tmp/reset_prop ro.secure 0 && setprop service.adb.tcp.port 5555 && start adbd) & stop adbd
